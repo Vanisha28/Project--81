c7cc9170ac460f19993fbf7efdffc1d12321e369
Vanisha28 <73533005+Vanisha28@users.noreply.github.com>
2022-05-25 22:09:55 +0530

    Add files via upload

---
+++ screens/CreatePost.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export default class CreatePost extends React.Component{
+  render(){
+    return(
+      <View>
+      <Text> 
+      Post page
+      </Text>
+      </View>
+    )
+  }
+}
---
+++ screens/Feed.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export default class Feed extends React.Component{
+  render(){
+    return(
+      <View>
+      <Text>
+      Feed page
+      </Text>
+      </View>
+
+      
+    )
+  }
+}
